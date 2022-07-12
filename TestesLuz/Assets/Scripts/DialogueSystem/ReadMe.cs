888f72cd01ebff2516babdc181b477271d0c22b8
Gustavo Herrera <gustavomherrera@gmail.com>
2022-07-12 11:03:05 -0300

    Grid
    O grid, pallets e tiles foram criados e inseridos

--- TestesLuz/Assets/Scripts/DialogueSystem/ReadMe.cs
+++ TestesLuz/Assets/Scripts/DialogueSystem/ReadMe.cs
@@ -4,11 +4,12 @@
 Dialogue UI - Canvas
 Typewriter Effect - Canvas
 ResponseHandler - Canvas
-Dialogue Activator - Objeto que terá dialogo
-Dialogue Response Events - Objeto que terá dialogo
+Dialogue Activator - Objeto que tera dialogo
+Dialogue Response Events - Objeto que tera dialogo
 
 -Objetos que precisam ser criados-
 
+Adicionar a tag "Player" ao objeto player
 Objeto em branco "Dialogue Box" dentro do Canvas
 Image dentro da Dialogue Box (para bg)
 Text(TMP) dentro da Dialogue Box (texto random)
@@ -20,6 +21,6 @@ Objeto em branco "Responses" dentro do Response Box(*Deve ser mantido desativado
     mudar o Child Alignment para "Upper Center"
     ativar o Control Child Size "width"
 Objeto Text(TMP) "OptionTeamplate" dentro do Responses(*Deve ser mantido desativado))
-    adicionar um bitão como componente
+    adicionar um botao como componente
  
  */
